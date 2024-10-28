0ef7ff534c7292df42cc36dbac42b76da8353554
Dmitriy <bodugad01@gmail.com>
2024-10-28 18:24:04 +0300

    Add the task from the laboratory work

---
+++ zad1v2.py
@@ -0,0 +1,17 @@
+import re
+
+fl = False
+while fl == False:
+    user_input = input("Введите оклад: ")
+    if re.fullmatch(r"(\d+)", user_input):
+        oklad = int(user_input)
+        fl = True
+    elif re.fullmatch(r"(\d+\.\d+)", user_input):
+        oklad = float(user_input)
+        fl = True
+    else:
+        print('Оклад должен быть положительным числом')
+
+kvartal_premia = oklad * 2/3
+summa_na_ruki = kvartal_premia * 0.87
+print(f'сумма на руки = {summa_na_ruki}, kvartal_premia = {kvartal_premia}')
